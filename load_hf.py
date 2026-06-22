bc8b1007b4bdf9669ba7e7e75df4af9f9e9bebfd
Omar <omarvx211@gmail.com>
2026-06-22 12:24:14 +0000

    dowloading a model

--- load_hf.py
+++ load_hf.py
@@ -1,21 +1,58 @@
-import os
-
-from datasets import load_dataset
-
+# # import os
 #
-# os.environ["HF_TOKEN"] = "hf_..."  # set via env or .env
-# dataset = load_dataset(
-#     "wikimedia/wikipedia", "20220301.en", split="train", streaming=True
-# )
+# from datasets import load_dataset
 #
-# for i, example in enumerate(dataset):
-#     print(example["title"])
-#     if i >= 4:
-#         break
+# #
+# # os.environ["HF_TOKEN"] = "hf_..."  # set via env or .env
+# # dataset = load_dataset(
+# #     "wikimedia/wikipedia", "20220301.en", split="train", streaming=True
+# # )
+# #
+# # for i, example in enumerate(dataset):
+# #     print(example["title"])
+# #     if i >= 4:
+# #         break
+#
+#
+# dataset = load_dataset("stanfordnlp/imdb", split="train")
+#
+# dataset.to_csv("imdb_train.csv")
+# dataset.to_json("imdb_train.json")
+# dataset.to_parquet("imdb_train.parquet")
+#
+#
+# split = dataset.train_test_split(test_size=0.2, seed=42)
+# train_val = split["train"].train_test_split(test_size=0.125, seed=42)
+#
+# val_ds = train_val["test"]
+# train_ds = train_val["train"]
+# test_ds = split["test"]
+#
+#
+# print(f"Train: {train_ds}, val: {len(val_ds)}, Test: {len(test_ds)}")
 
 
-dataset = load_dataset("stanfordnlp/imdb", split="train")
+from huggingface_hub import hf_hub_download, snapshot_download
 
-dataset.to_csv("imdb_train.csv")
-dataset.to_json("imdb_train.json")
-dataset.to_parquet("imdb_train.parquet")
+model_path = hf_hub_download(
+    repo_id="sentence-transformers/all-MiniLM-L6-v2", filename="config.json"
+)
+print(f"Cached at: {model_path}")
+
+model_dir = snapshot_download("sentence-transformers/all-MiniLM-L6-v2")
+print(f"Full model at: {model_dir}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
